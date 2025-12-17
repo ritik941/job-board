720063ae418df5953e73614c5e1ad9859247d5b2
Ritik Sharma <ritiksharma1205@gmail.com>
2025-12-17 10:56:56 +0530

    Fix: Render compatible host and port

--- app.py
+++ app.py
@@ -228,6 +228,9 @@ def reject_applicant(app_id):
 
 # CREATE TABLES
 
+
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

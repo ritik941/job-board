e69851e14aab96789c12d8eb1bb9322ba3de4209
Ritik Sharma <ritiksharma1205@gmail.com>
2025-12-17 17:45:10 +0530

    CHANGES IN MODELS

--- models.py
+++ models.py
@@ -3,13 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "user"   # 🔥 THIS LINE FIXES EVERYTHING
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False)
+
+    # Relationship: a user can have many applications
+    applications = db.relationship("Application", backref="user", lazy=True)
+    # Relationship: a recruiter can post many jobs
+    jobs_posted = db.relationship("Job", backref="recruiter", lazy=True)
 
 
 class Job(db.Model):
@@ -21,6 +26,9 @@ class Job(db.Model):
     location = db.Column(db.String(100))
     posted_by = db.Column(db.Integer, db.ForeignKey("user.id"))
 
+    # Relationship: a job can have many applications
+    applications = db.relationship("Application", backref="job", lazy=True)
+
 
 class Application(db.Model):
     __tablename__ = "application"
@@ -30,3 +38,4 @@ class Application(db.Model):
     job_id = db.Column(db.Integer, db.ForeignKey("job.id"))
     cover_letter = db.Column(db.Text)
     status = db.Column(db.String(20), default="pending")
+
